fd816da191acd3cc7907d9df61cb5ccf7adc2737
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2025-01-25 17:37:34 +0900

    Update: migrate tailwind v3 to v4

--- react-front/postcss.config.js
+++ react-front/postcss.config.js
@@ -1,6 +1,15 @@
+/* 変更前！
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+  },
+}
+*/
+
+// 変更後！
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
   },
 }
