7c57cade736843331520b024301945dfb85ce82f
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2025-01-25 18:26:17 +0900

    update

--- react-front/vite.config.ts
+++ react-front/vite.config.ts
@@ -1,3 +1,4 @@
+/* 修正前！
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +6,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+*/
+
+// 修正後！
+import path from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
